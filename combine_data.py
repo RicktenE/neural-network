eda2c24c594be7f71bfb3964818135d7b8b0e34d
RicktenE <r.teneikelder@gmail.com>
2021-03-12 11:24:12 +0100

    ordering combined data goes wrong. probably something how the frequencies are read out and how the data is combined.

--- combine_data.py
+++ combine_data.py
@@ -24,8 +24,10 @@ x_45 = np.concatenate((x_45_1, x_45_2), axis=0)
 
 print('shape x_45_1', str(x_45_1.shape))
 print('shape x_45_2', str(x_45_2.shape))
-print('shape x_45 combined 11-06', str(x_45.shape))
 
+
+x_45 = np.transpose(x_45)# to keep all data in the same shape as the non combined data
+print('shape x_45 combined 11-06', str(x_45.shape))
 np.savetxt(data_folder + 'x_data_45.txt', x_45, delimiter='\t', newline='\n')
 
 ##########Here 6 1 and 6 2 from 11-06 are combined into x_data_6.txt
@@ -44,8 +46,9 @@ x_6 = np.concatenate((x_6_1, x_6_2), axis=0)
 
 print('shape x_6_1', str(x_6_1.shape))
 print('shape x_6_2', str(x_6_2.shape))
-print('shape x_6 combined 11-06', str(x_6.shape))
 
+x_6 = np.transpose(x_6)# to keep all data in the same shape as the non combined data
+print('shape x_6 combined 11-06', str(x_6.shape))
 np.savetxt( data_folder + 'x_data_6 .txt', x_6, delimiter='\t', newline='\n')
 
 ########### Here mixed 1 and mixed 2 from 11-06 are combined into x_data_mix.txt
@@ -64,8 +67,9 @@ x_mix = np.concatenate((x_mix_1, x_mix_2), axis=0)
 
 print('shape x_mix_1', str(x_mix_1.shape))
 print('shape x_mix_2', str(x_mix_2.shape))
-print('shape x_mix 11-06 (4,5um and 6um)', str(x_mix.shape))
+x_mix = np.transpose(x_mix)# to keep all data in the same shape as the non combined data
 
+print('shape x_mix 11-06 (4,5um and 6um)', str(x_mix.shape))
 np.savetxt(data_folder + 'x1_data_mix.txt', x_mix, delimiter='\t', newline='\n')
 
 ###########Creating combined data files from 12-07
@@ -86,8 +90,9 @@ x_mix = np.concatenate((x_mix_1, x_mix_2), axis=0)
 # printing the shapes to check if everything goes well
 print('shape x_mix_1', str(x_mix_1.shape))
 print('shape x_mix_2', str(x_mix_2.shape))
-print('shape x_mix 12-07 (5um,6um,7um)', str(x_mix.shape))
 
+x_mix = np.transpose(x_mix)# to keep all data in the same shape as the non combined data
+print('shape x_mix 12-07 (5um,6um,7um)', str(x_mix.shape))
 np.savetxt(data_folder + 'x1_data_mix.txt', x_mix, delimiter='\t', newline='\n')
 
 
@@ -120,8 +125,9 @@ x_5 = np.concatenate((x_5_2, x_5_3), axis=0)
 # print('shape x_5_1', str(x_5_1.shape))
 print('shape x_5_2', str(x_5_2.shape))
 print('shape x_5_3', str(x_5_3.shape))
-print('shape x_5 combined all dates', str(x_5.shape))
 
+x_5 = np.transpose(x_5)# to keep all data in the same shape as the non combined data
+print('shape x_5 combined all dates', str(x_5.shape))
 data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\combined\\"
 np.savetxt(data_folder + 'x_data_5.txt', x_5, delimiter='\t', newline='\n')
 
@@ -131,7 +137,8 @@ data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\20201106\\"
 read_label = data_folder + "x_data_6.txt"
 l, data = read_txt(read_label)
 # x_6_1 = np.transpose(data)
-x_6_1 = np.array(data) # this is already transposed from the previous combining of the data of 6um of 11-06
+x_6_1 = np.array(data)
+# print('shape x_6_1 combined 11-06 read out', str(x_6_1.shape))
 
 data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\20201117\\"
 read_label = data_folder + "x_data_6.txt"
@@ -151,6 +158,9 @@ x_6 = np.concatenate((x_6_1, x_6_2, x_6_3), axis=0)
 print('shape x_6_1', str(x_6_1.shape))
 print('shape x_6_2', str(x_6_2.shape))
 print('shape x_6_3', str(x_6_3.shape))
+
+
+x_6 = np.transpose(x_6)# to keep all data in the same shape as the non combined data
 print('shape x_6 combined all dates', str(x_6.shape))
 
 data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\combined\\"
@@ -182,8 +192,10 @@ x_7 = np.concatenate((x_7_2, x_7_3), axis=0)
 # print('shape x_7_1', str(x_7_1.shape))
 print('shape x_7_2', str(x_7_2.shape))
 print('shape x_7_3', str(x_7_3.shape))
-print('shape x_7 combined all dates', str(x_7.shape))
 
+
+x_7 = np.transpose(x_7) # to keep all data in the same shape as the non combined data
+print('shape x_7 combined all dates', str(x_7.shape))
 data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\combined\\"
 np.savetxt(data_folder + 'x_data_7.txt', x_7, delimiter='\t', newline='\n')
 
@@ -194,8 +206,8 @@ np.savetxt(data_folder + 'x_data_7.txt', x_7, delimiter='\t', newline='\n')
 ## data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\20201106\\"
 ## read_label = data_folder + "x_data_6.txt"
 ## l, data = read_txt(read_label)
-## # x_6_1 = np.transpose(data)
-## x_6_1 = np.array(data) # this is already transposed from the previous combining of the data of 6um of 11-06
+# # x_6_1 = np.transpose(data)
+###x_6_1 = np.array(data) # this is already transposed from the previous combining of the data of 6um of 11-06
 
 data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\20201117\\"
 read_label = data_folder + "x1_data_mix.txt"
@@ -206,8 +218,8 @@ x_mix_2 = np.transpose(data)
 data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\20201207\\"
 read_label = data_folder + "x1_data_mix.txt"
 l, data = read_txt(read_label)
-# x_mix_3 = np.transpose(data)
-x_mix_3 = np.array(data) # This data is already transposed from the combining of the mixed files in 12-07
+x_mix_3 = np.transpose(data)
+# x_mix_3 = np.array(data)
 # x_mix = np.concatenate((x_mix_1, x_mix_2, x_mix_3), axis=0)
 x_mix = np.concatenate((x_mix_2, x_mix_3), axis=0)
 
@@ -217,8 +229,10 @@ x_mix = np.concatenate((x_mix_2, x_mix_3), axis=0)
 print('shape x_mix_2', str(x_mix_2.shape))
 print('shape x_mix_3', str(x_mix_3.shape))
 # print('shape x1_data_mix combined all dates', str(x_mix.shape))
-print('shape x1_data_mix combined 11-17 and 12-07', str(x_mix.shape))
 
+
+x_mix = np.transpose(x_mix) # to keep all data in the same shape as the non combined data
+print('shape x1_data_mix combined 11-17 and 12-07', str(x_mix.shape))
 data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\combined\\"
 np.savetxt(data_folder + 'x1_data_mix.txt', x_mix, delimiter='\t', newline='\n')
 
--- prep.py
+++ prep.py
@@ -116,7 +116,7 @@ def make_training_set_allfreq_os(data_folder,
     read_label = data_folder + "x_data_5.txt" # Name of the data for 5 micrometer beads
     l, data = read_txt(read_label)
     # print(len(data))
-    x5 = np.transpose(data) #for old data
+    x5 = np.transpose(data)
     # x5 = data
     # print("shape x5 before concatenate" + str(x5.shape))
     leng5 = int(len(x5[:, SIZE]) / 6) # Size of the arrays depending on the window size chosen in matlab
@@ -136,7 +136,7 @@ def make_training_set_allfreq_os(data_folder,
     # similar for 6 and 7um
     read_label = data_folder + "x_data_6.txt"
     l, data = read_txt(read_label)
-    x6 = np.transpose(data) #for old data
+    x6 = np.transpose(data)
     # x6 = data
     leng6 = int(len(x6[:, SIZE]) / 6)
     # leng6=leng
@@ -154,7 +154,7 @@ def make_training_set_allfreq_os(data_folder,
 
     read_label = data_folder + "x_data_7.txt"
     l, data = read_txt(read_label)
-    x7 = np.transpose(data) #for old data
+    x7 = np.transpose(data)
     # x7 = data
     leng7 = int(len(x7[:, SIZE]) / 6)
     # print(leng7)
@@ -259,7 +259,7 @@ def prep_test_data_allfreq(data_folder_test):  # prepares the mixed beads data b
     read_label = data_folder_test + "x1_data_mix.txt"
     # read_label = data_folder_test + "x_data_6.txt"
     l, data = read_txt(read_label)
-    x_mix = np.transpose(data) #for old data
+    x_mix = np.transpose(data)
     # x_mix = data
     # wdw = 500  # for Old data
     wdw =500
@@ -305,9 +305,9 @@ data_folder = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\combined\\"
 
 ####################Test data
 # data_folder_test = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\20201106\\"
-# data_folder_test = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\20201117\\"
+data_folder_test = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\20201117\\"
 # data_folder_test = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\20201207\\"
-data_folder_test = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\combined\\"
+# data_folder_test = "D:\\Saxion\\Jaar 4\\Bachelor Thesis\\Data Rick\\combined\\"
 
 
 x_test=prep_test_data_allfreq(data_folder_test)
@@ -321,6 +321,8 @@ np.save(save_test_file,x_test)
 
 ################## Train data
 x_train, y_train, x_eval, y_eval=make_training_set_allfreq_os(data_folder,0.15)
+# x_train, y_train, x_eval, y_eval=make_training_set(data_folder,0.15)
+# make_training_set
 #
 # print(np.shape(x_train))
 #
